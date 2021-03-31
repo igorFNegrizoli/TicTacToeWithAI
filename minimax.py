c37bb6cebcdef543358f1c30e2f1c2c5aa153efa
igorFNegrizoli <igor.negrizoli@gmail.com>
2021-03-31 12:06:14 -0300

    Funções relacionadas à busca de profundidade foram modularidas para ticTacToe.py

--- minimax.py
+++ minimax.py
@@ -1,33 +1,5 @@
 from math import inf
-
-def winner(tabuleiro):
-    winner = " "
-
-    for i in tabuleiro:
-        if i.count(i[0]) == len(i) and i[0] != " ":
-            return i[0]
-            
-    transposta = [list(i) for i in zip(*tabuleiro)]
-    for i in transposta:
-        if i.count(i[0]) == len(i) and i[0] != " ":
-            return i[0]
-
-    if (tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2]) and tabuleiro[1][1] != " ":
-        return tabuleiro[1][1]
-    
-    if (tabuleiro[0][2] == tabuleiro[1][1] == tabuleiro[2][0]) and tabuleiro[1][1] != " ":
-        return tabuleiro[1][1]
-
-    espacosOcupados = 0
-    for i in tabuleiro:
-        for j in i:
-            if j != " ":
-                espacosOcupados += 1
-    
-    if espacosOcupados == 9:
-        return "Empate"
-
-    return "-"
+from ticTacToe import *
 
 def melhorJogada(tabuleiro):
     melhorScore = -inf
@@ -70,15 +42,9 @@ def minimax(tabuleiro, prof, maximizacao):
                     melhorScore = min([score, melhorScore])
         return melhorScore
 
-def printTabuleiro(tabuleiro):
-    for i in tabuleiro:
-        print(*i, sep = "|")
-    print()
-
-tabuleiro = [[" "," "," "],[" "," "," "],[" "," "," "]]
-ia = "X"
-jogador = "O"
-vez = "ia"
+##
+#Main
+##
 
 while winner(tabuleiro) == "-":
     printTabuleiro(tabuleiro)
